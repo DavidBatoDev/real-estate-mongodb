f4eed5b99d81057cfb62b9441b0cd37d3b5d33df
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-04-01 13:40:38 +0800

    Add .env file and update package.json and index.js connected to the Database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const URI = process.env.MONGODB_URI
+
+mongoose.connect(URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.log('Error:', error);
+})
 
 const app = express();
 
