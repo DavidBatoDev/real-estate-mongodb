a91ea16a63e906de7278e720ca7a187667ec2941
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-04-01 17:17:19 +0800

    Update styles and forms in SignUp and Signin pages

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>
-      
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder="Username" 
+          className='p-3 border-2 shadow-md
+          rounded-lg outline-none' />
+        <input type="email" placeholder="Email" 
+          className='p-3 border-2 shadow-md
+          rounded-lg outline-none' />
+        <input type="password" placeholder="Password" 
+          className='p-3 border-2 shadow-md
+          rounded-lg outline-none' />
+        <button className='text-white hover:bg-orange-500 bg-orange-400 p-3 rounded-lg shadow-md'>
+          Sign Up
+        </button>
+        <button className="hover:bg-slate-100 border border-black p-3 rounded-lg shadow-md">
+          Sign Up with Google
+        </button>
+      </form>
+      <p className='mt-5'>Have an account? <a className='underline text-orange-700' href="signin">Sign In</a></p>
     </div>
   )
 }
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Signin = () => {
   return (
-    <div>
-      SignIn
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="email" placeholder="Email" 
+          className='p-3 border-2 shadow-md
+          rounded-lg outline-none' />
+        <input type="password" placeholder="Password" 
+          className='p-3 border-2 shadow-md
+          rounded-lg outline-none' />
+        <button className='text-white hover:bg-orange-500 bg-orange-400 p-3 rounded-lg shadow-md'>
+          Sign In
+        </button>
+        <button className="hover:bg-slate-200 border border-black p-3 rounded-lg shadow-md">
+          Sign In with Google
+        </button>
+      </form>
+      <p className='mt-5'>Don't have an account? <a className='underline text-orange-700' href="signup">Sign Up</a></p>
     </div>
   )
 }
