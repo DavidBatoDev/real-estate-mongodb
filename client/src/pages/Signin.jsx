cade80d62e8c6b38727f4b22732576ec2ef1a898
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-04-02 19:07:16 +0800

    Refactor Signin component to use React hooks and handle form submission

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,18 +1,72 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import {useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Signin = () => {
+  const navigate = useNavigate()
+  const [formBody, setFormBody] = useState({
+    email: '',
+    password: ''
+  })
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleChange = e => {
+    setFormBody({
+      ...formBody,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      // Fetching data from the backend
+      setLoading(true)
+      const res = await fetch('/api/auth/signin', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formBody)
+      })
+      // Parsing the response
+      const data = await res.json()
+      if (data.success === false) {
+        setError(data.message)
+        setLoading(false)
+        return
+      }
+      setLoading(false)
+      setError('')
+      navigate('/') // Redirect to the home page
+
+    } catch (error) {
+      setLoading(false)
+      console.log(error.message)
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="email" placeholder="Email" 
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input 
+          value={formBody.email} 
+          onChange={handleChange}
+          name='email'
+          type="email" 
+          placeholder="Email" 
           className='p-3 border-2 shadow-md
           rounded-lg outline-none' />
-        <input type="password" placeholder="Password" 
+        <input 
+          value={formBody.password}
+          onChange={handleChange}
+          name='password'
+          type="password" 
+          placeholder="Password" 
           className='p-3 border-2 shadow-md
           rounded-lg outline-none' />
-        <button className='text-white hover:bg-orange-500 bg-orange-400 p-3 rounded-lg shadow-md'>
+        <button type='submit' className='text-white hover:bg-orange-500 bg-orange-400 p-3 rounded-lg shadow-md'>
           Sign In
         </button>
         <button className="hover:bg-slate-200 border border-black p-3 rounded-lg shadow-md">
@@ -26,6 +80,7 @@ const Signin = () => {
           </span>
         </Link>
       </div>
+      {error && <div className='text-red-500 text-center mt-3'>{error}</div>}
     </div>
   )
 }
