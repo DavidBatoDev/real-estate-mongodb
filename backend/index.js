6b5c0d765f22b0dd378eb1bf6ec9af1aca0b4d21
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-04-09 23:23:01 +0800

    Create Listing API ROUTE

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user-route.js';
 import authRouter from './routes/auth-route.js';
+import listingRouter from './routes/listing-route.js'
 import cookieParser from 'cookie-parser';
 
 // constants
@@ -30,6 +31,7 @@ app.listen(3000, () => {
 // routes
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter)
 
 // error validation middleware
 app.use((err, req, res, next) => {
