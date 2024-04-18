21d0097170bc45afd4d132ad5e35b625084495e9
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-04-18 20:29:46 +0800

    Completed Update Listing Route

--- backend/controllers/listing-controller.js
+++ backend/controllers/listing-controller.js
@@ -1,27 +1,49 @@
-import Listing from '../models/listing-model.js'
-import { errorHandler } from '../utils/error.js'
+    import Listing from '../models/listing-model.js'
+    import { errorHandler } from '../utils/error.js'
 
-export const createListing = async (req, res, next) => {
-    try {
-        const listing = await Listing.create(req.body)
-        return res.status(201).json(listing)
-    } catch (error) {
-        next(error)
+    export const createListing = async (req, res, next) => {
+        try {
+            const listing = await Listing.create(req.body)
+            return res.status(201).json(listing)
+        } catch (error) {
+            next(error)
+        }
     }
-}
 
-export const deleteListing = async (req, res, next) => {
-    const listing = await Listing.findById(req.params.id)
-    try {
-        if (listing.userRef !== req.user.id) {
-            return next(errorHandler(401, 'Unauthorized'))
+    export const deleteListing = async (req, res, next) => {
+        const id = req.params.id
+        const listing = await Listing.findById(id)
+        try {
+            if (listing.userRef !== req.user.id) {
+                return next(errorHandler(401, 'Unauthorized'))
+            }
+            if (!listing) {
+                return next(errorHandler(404, 'Listing not found'))
+            }
+            await Listing.findByIdAndDelete(id)
+            res.status(200).json({message: 'Listing deleted!'})
+        } catch (error) {
+            next(error)
         }
-        if (!listing) {
-            return next(errorHandler(404, 'Listing not found'))
-        }
-        await Listing.findByIdAndDelete(req.params.id)
-        res.status(200).json({message: 'Listing deleted!'})
-    } catch (error) {
-        next(error)
     }
-}
+
+    export const updateListing = async (req, res, next) => {
+        const id = req.params.id
+        const listing = await Listing.findById(id)
+        try {
+            if (listing.userRef !== req.user.id) {
+                return next(errorHandler(401, 'Unauthorized'))
+            }
+            if (!listing) {
+                return next(errorHandler(404, 'Listing not found'))
+            }
+            const updatedListing = await Listing.findByIdAndUpdate(
+                id, 
+                req.body, 
+                {new: true}
+            )
+            res.status(200).json(updatedListing)
+        } catch (error) {
+            next(error)
+        }
+    }
