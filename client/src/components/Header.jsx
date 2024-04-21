dffe7095d99a6ad794fd9a7ef4d402dac4f55b18
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-04-21 18:45:32 +0800

    updateHeaderSearchForm

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 const Header = () => {
+    const navigate = useNavigate()
     const {currentUser} = useSelector(state => state.user)
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`)
+    }
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const searchTermFormUrl = urlParams.get('searchTerm');
+        if (searchTermFormUrl) {
+            setSearchTerm(searchTermFormUrl)
+        }
+    }, [window.location.search])
+    
   return (
     <header className='bg-white shadow-md border-b-orange-400 border-2'>
         <div className='flex items-center justify-between max-w-6xl mx-auto p-3'>
@@ -14,10 +33,17 @@ const Header = () => {
                     <span className='text-orange-400'>Estate</span>
                 </h1>
             </Link>
-            <form className='p-3 rounded-lg border-2 flex items-center'>
-                <input type="text" placeholder='Search...'
-                className='bg-transparent outline-none w-32 sm:w-64' />
-                <FaSearch className='text-orange-400 cursor-pointer' />
+            <form onSubmit={handleSearch} className='p-3 rounded-lg border-2 flex items-center'>
+                <input 
+                    type="text" 
+                    placeholder='Search...'
+                    className='bg-transparent outline-none w-32 sm:w-64' 
+                    onChange={e => setSearchTerm(e.target.value)}
+                    value={searchTerm}
+                />
+                <button type='submit'>
+                    <FaSearch className='text-orange-400 cursor-pointer' />
+                </button>
             </form>
             <div className='flex gap-4 items-center'>
                 <Link to="/">
